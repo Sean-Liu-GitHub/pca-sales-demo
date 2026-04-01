ca5553c87570ec36910a10493c8b56e80f631fde
sean.liu <sean.liu.job@gmail.com>
2026-04-01 23:37:15 +0800

    fix: hardcoded model

--- dbt/models/marts/agg_daily_sales.sql
+++ dbt/models/marts/agg_daily_sales.sql
@@ -6,7 +6,7 @@ with daily_sales as (
         avg(premium_amount)::integer as avg_premium,
         count(distinct agent_id) as active_agents,
         max(event_ts) as last_updated
-    from staging.stg_policy_sales
+    from {{ ref('stg_policy_sales') }}
     group by 1
 )
 
