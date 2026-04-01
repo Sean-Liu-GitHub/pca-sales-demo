0d9fd7cd1dd37eee8fc777223a93691acde141f8
sean.liu <sean.liu.job@gmail.com>
2026-04-01 11:50:19 +0800

    fix: simplify query logic

--- dbt/models/marts/agg_daily_sales_by_agent.sql
+++ dbt/models/marts/agg_daily_sales_by_agent.sql
@@ -1,14 +1,11 @@
 with daily_sales_by_agent as (
     select
-        a.agent_id,
-        a.agent_name as agent_name,
-        s.date_id,
-        sum(s.premium_amount) as daily_premium,
-        count(s.*) as daily_count
-    from {{ ref('stg_policy_sales') }} s
-    join {{ ref('dim_agents') }} a
-        on s.agent_id = a.agent_id
-    group by 1, 2, 3
+        agent_id,
+        date_id,
+        sum(premium_amount) as daily_premium,
+        count(*) as daily_count
+    from {{ ref('stg_policy_sales') }}
+    group by 1, 2
 )
 
 select * from daily_sales_by_agent
--- dbt/models/marts/agg_hourly_sales_by_region.sql
+++ dbt/models/marts/agg_hourly_sales_by_region.sql
@@ -1,13 +1,11 @@
 with hourly_sales_by_region as (
     select
-        date_trunc('hour', s.event_ts) as hour_ts,
-        r.region_name as region,
-        sum(s.premium_amount) as total_premium,
-        count(s.*) as policy_count,
-        avg(s.premium_amount)::integer as avg_premium
-    from {{ ref('stg_policy_sales') }} s
-    inner join {{ ref('dim_regions') }} r
-        on s.region_id = r.region_id
+        date_trunc('hour', event_ts) as hour_ts,
+        region_id,
+        sum(premium_amount) as total_premium,
+        count(*) as policy_count,
+        avg(premium_amount)::integer as avg_premium
+    from {{ ref('stg_policy_sales') }}
     group by 1, 2
 )
 
